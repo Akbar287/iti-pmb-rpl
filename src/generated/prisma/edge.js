86dd5c910c41801d958d0a4efebb5f0c5df655c8
Tim EKPB <timkpb2023@lampungprov.go.id>
2025-04-27 01:47:37 +0700

    fixing auth

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -805,11 +805,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:Akbar287@localhost:5432/rpl_pmb?schema=public"
+        "value": null
       }
     }
   },
--- src/stores/AuthStores.tsx
+++ src/stores/AuthStores.tsx
@@ -1,48 +1,49 @@
-import { JenisKelamin, Jenjang, User, Userlogin } from "@/generated/prisma";
-import { create } from "zustand";
+import { JenisKelamin, Jenjang, User, Userlogin } from '@/generated/prisma'
+import { create } from 'zustand'
 
 type State = {
-  user: User;
-  userlogin: Userlogin;
-};
+    user: User
+    userlogin: Userlogin
+}
 
 type Actions = {
-  setUser: (user: User) => void;
-  setUserlogin: (userlogin: Userlogin) => void;
-};
+    setUser: (user: User) => void
+    setUserlogin: (userlogin: Userlogin) => void
+}
 
 const useCountStore = create<State & Actions>((set, get) => ({
-  user: {
-    UserId: "",
-    AlamatId: "",
-    Nama: "",
-    Email: "",
-    EmailVerifiedAt: null,
-    TempatLahir: null,
-    TanggalLahir: null,
-    JenisKelamin: JenisKelamin.PRIA,
-    PendidikanTerakhir: Jenjang.TIDAK_TAMAT_SD,
-    Avatar: "",
-    Agama: "",
-    Telepon: "",
-    NomorWa: "",
-    NomorHp: "",
-    RememberToken: "",
-    CreatedAt: new Date(),
-    UpdatedAt: new Date(),
-    DeletedAt: null,
-  },
-  userlogin: {
-    UserloginId: "",
-    UserId: "",
-    Username: "",
-    Password: "",
-    CreatedAt: new Date(),
-    UpdatedAt: new Date(),
-    DeletedAt: null,
-  },
-  setUser: (user) => set({ user: user }),
-  setUserlogin: (userlogin) => set({ userlogin: userlogin }),
-}));
+    user: {
+        UserId: '',
+        AlamatId: '',
+        Nama: '',
+        Email: '',
+        EmailVerifiedAt: null,
+        TempatLahir: null,
+        TanggalLahir: null,
+        JenisKelamin: JenisKelamin.PRIA,
+        PendidikanTerakhir: Jenjang.TIDAK_TAMAT_SD,
+        Avatar: '',
+        Agama: '',
+        Telepon: '',
+        NomorWa: '',
+        NomorHp: '',
+        RememberToken: '',
+        CreatedAt: new Date(),
+        UpdatedAt: new Date(),
+        DeletedAt: null,
+    },
+    userlogin: {
+        UserloginId: '',
+        UserId: '',
+        Username: '',
+        Password: '',
+        Credential: '',
+        CreatedAt: new Date(),
+        UpdatedAt: new Date(),
+        DeletedAt: null,
+    },
+    setUser: (user) => set({ user: user }),
+    setUserlogin: (userlogin) => set({ userlogin: userlogin }),
+}))
 
-export default useCountStore;
+export default useCountStore
