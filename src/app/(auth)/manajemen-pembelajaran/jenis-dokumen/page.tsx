f97071a527a0cb5492a65f723a6b18c40e6a860e
Tim EKPB <timkpb2023@lampungprov.go.id>
2025-05-17 00:08:34 +0700

    fixing bug

--- src/app/(auth)/manajemen-pembelajaran/jenis-dokumen/page.tsx
+++ src/app/(auth)/manajemen-pembelajaran/jenis-dokumen/page.tsx
@@ -1,10 +1,8 @@
 import JenisDokumen from '@/components/manajemen-pembelajaran/JenisDokumenComponent'
-import { prisma } from '@/lib/prisma'
 import React from 'react'
 
 const Page = async () => {
-    const JenisDokumenServer = await prisma.jenisDokumen.findMany()
-    return <JenisDokumen JenisDokumenServer={JenisDokumenServer} />
+    return <JenisDokumen />
 }
 
 export default Page
